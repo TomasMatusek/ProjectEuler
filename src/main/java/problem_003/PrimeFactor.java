30b8df3beb32de795cba5e280dd0cfa78434a6ae
Matusek Tomas <matusek@foundation.sk>
2019-08-11 17:08:25 +0200

    Problem 003 - solution

--- src/main/java/problem_003/PrimeFactor.java
+++ src/main/java/problem_003/PrimeFactor.java
@@ -1,16 +1,65 @@
 package problem_003;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactor {
 
-    public static int getLargestPrimeFactorOf(int number) {
-        return 0;
+    private List<Integer> primesOfNumber = new ArrayList<>();
+    private long number;
+
+    public PrimeFactor(long number) {
+        this.number = number;
     }
 
-    public static List<Integer> getPrimeFactorsOfNumber(int number) {
-        return Collections.emptyList();
+    public List<Integer> getPrimeFactorsOfNumber() {
+        return getPrimeFactorsOfNumber(1);
+    }
+
+    private List<Integer> getPrimeFactorsOfNumber(int primeIndex) {
+        // Current prime number
+        int primeNumber = getNthPrimeNumber(primeIndex);
+
+        // If we hit bottom return result
+        if (number < 2) {
+            return primesOfNumber;
+        }
+
+        // Check if number can be divided by prime number
+        // If yes divide number by this prime number and add it in the list
+        if (number % primeNumber == 0) {
+            primesOfNumber.add(primeNumber);
+            number /= primeNumber;
+            getPrimeFactorsOfNumber(getNthPrimeNumber(1));
+        }
+        // If not, then try to divide number by next prime number
+        else {
+            getPrimeFactorsOfNumber(++primeIndex);
+        }
+
+        // Return final list
+        return primesOfNumber;
+    }
+
+    public static int getLargestPrimeFactorOf(long number) {
+        List<Integer> primeFactors = new PrimeFactor(number).getPrimeFactorsOfNumber();
+        return primeFactors.get(primeFactors.size() - 1);
+    }
+
+    public static Integer getNthPrimeNumber(int primeNumberPosition) {
+        List<Integer> primes = getListOfFirstNPrimes(primeNumberPosition);
+        return primes.get(primes.size() - 1);
+    }
+
+    public static List<Integer> getListOfFirstNPrimes(int numberOfPrimes) {
+        List<Integer> primes = new ArrayList<>();
+        int i = 0;
+        while (true) {
+            if (primes.size() == numberOfPrimes) break;
+            if (isPrimeNumber(i)) primes.add(i);
+            i++;
+        }
+        return primes;
     }
 
     public static boolean isPrimeNumber(int number) {
--- src/test/java/problem_003/PrimeFactorTest.java
+++ src/test/java/problem_003/PrimeFactorTest.java
@@ -3,21 +3,43 @@ package problem_003;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class PrimeFactorTest {
+import java.util.List;
+
+class PrimeFactorTest {
 
     @Test
-    public void calculateLargesPrimeFactorOfNumber_returnsLargestPrimeFactor() {
+    void calculateLargesPrimeFactorOfNumber_returnsLargestPrimeFactor() {
+        Assertions.assertEquals(6857, PrimeFactor.getLargestPrimeFactorOf(600851475143L));
+    }
+
+    @Test
+    void calculateLargesPrimeFactorOfNumber_returnsLargestPrimeFactor2() {
         Assertions.assertEquals(29, PrimeFactor.getLargestPrimeFactorOf(13195));
     }
 
     @Test
-    public void getPrimeFactorOfGivenNumber_returnCorrectListPrimeFactors() {
+    void getPrimeFactorOfGivenNumber_returnCorrectListPrimeFactors2() {
         int[] expected = { 5,7,13,29 };
-        Assertions.assertArrayEquals(expected, PrimeFactor.getPrimeFactorsOfNumber(13195).stream().mapToInt(i -> i).toArray());
+        List<Integer> primesOfNumber = new PrimeFactor(13195).getPrimeFactorsOfNumber();
+        Assertions.assertArrayEquals(expected, primesOfNumber.stream().mapToInt(i -> i).toArray());
     }
 
     @Test
-    public void isPrimeNumberTest() {
+    void getPrimeFactorOfGivenNumber_returnCorrectListPrimeFactors() {
+        int[] expected = { 5,7,13,29 };
+        List<Integer> primesOfNumber = new PrimeFactor(13195).getPrimeFactorsOfNumber();
+        Assertions.assertArrayEquals(expected, primesOfNumber.stream().mapToInt(i -> i).toArray());
+    }
+
+    @Test
+    void getFirstNPrimeFactor() {
+        int[] expected = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71 };
+        List<Integer> primes = PrimeFactor.getListOfFirstNPrimes(20);
+        Assertions.assertArrayEquals(expected, primes.stream().mapToInt(i -> i).toArray());
+    }
+
+    @Test
+    void isPrimeNumberTest() {
         Assertions.assertTrue(PrimeFactor.isPrimeNumber(2));
         Assertions.assertTrue(PrimeFactor.isPrimeNumber(3));
         Assertions.assertTrue(PrimeFactor.isPrimeNumber(5));
